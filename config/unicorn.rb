ede3052b25325afaa5122fe8131162e42e97ce93
shunking-tech <shnktech@gmail.com>
2019-08-29 20:22:19 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -11,7 +11,7 @@ working_directory app_path
 pid "#{app_path}/tmp/pids/unicorn.pid"
 
 #ポート番号を指定
-listen 3000
+listen "#{app_path}/tmp/sockets/unicorn.sock"
 
 #エラーのログを記録するファイルを指定
 stderr_path "#{app_path}/log/unicorn.stderr.log"
@@ -32,7 +32,7 @@ stdout_path "#{app_path}/log/unicorn.stdout.log"
 # pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 
 # #ポート番号を指定
-# listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+# listen "#{app_path}/tmp/sockets/unicorn.sock"
 
 # #エラーのログを記録するファイルを指定
 # stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
