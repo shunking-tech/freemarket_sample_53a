9cf515a8dab4602b5889311899a6e809b2c1484c
shunking-tech <shnktech@gmail.com>
2019-08-29 18:58:18 +0900

    production.rbを編集

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,5 @@
+server '3.113.132.120', user: 'ec2-user', roles: %w{app db web}
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
