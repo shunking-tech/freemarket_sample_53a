8f1902fe14fc787e5725e52668e24c998324c783
seita_matsumoto <gyabooo@gmail.com>
2019-08-31 02:31:39 +0900

    本番環境のみBasic認証追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth, if: :production?
+
+  private
+
+  def production?
+    Rails.env.production?
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == Rails.application.credentials.basic[:user]
+      password == Rails.application.credentials.basic[:password]
+    end
+  end
 end
+
