c57551e5a66f687a87da52dd9d7b340ad96e4348
homarinn <homarinn1230@gmail.com>
2019-09-03 20:34:06 +0900

    Add Columns To Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_01_080143) do
+ActiveRecord::Schema.define(version: 2019_09_02_120638) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,6 +20,20 @@ ActiveRecord::Schema.define(version: 2019_09_01_080143) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname"
+    t.string "avatar"
+    t.text "description"
+    t.string "first_name"
+    t.string "first_name_kana"
+    t.string "last_name"
+    t.string "last_name_kana"
+    t.string "birth_date"
+    t.string "phone_number"
+    t.string "prefecture"
+    t.string "city"
+    t.string "house_address"
+    t.string "building_name"
+    t.string "zipcode"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
