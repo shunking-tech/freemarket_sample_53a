1254c73212d1068090bb0143a732a6f8618102ec
homarinn <homarinn1230@gmail.com>
2019-09-13 21:12:28 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   root 'items#index'
+
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'
+  }
+
+  devise_scope :user do
+    get "sign_in", :to => "users/sessions#new"
+    get "sign_out", :to => "users/sessions#destroy"
+  end
+
   resources :users, only: :show do
     resources :creditcards, only: [:index, :new, :create]
   end
