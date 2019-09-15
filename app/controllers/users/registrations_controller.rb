8a1bd54cde611e582c5f64b44bfcb373fd5466e9
homarinn <homarinn1230@gmail.com>
2019-09-15 23:42:10 +0900

    create params

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,7 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
-  before_action :configure_account_update_params, only: [:update]
+  # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   def new
@@ -42,19 +42,24 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,
-                                                       :first_name,
-                                                       :first_name_kana,
-                                                       :last_name,
-                                                       :last_name_kana,
-                                                       :birth_date,
-                                                       :phone_number])
+    birth_date = "#{params[:year]}/#{params[:month]}/#{params[:day]}"
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:nickname,
+               :email,
+               :password,
+               :password_confirmation,
+               :first_name,
+               :first_name_kana,
+               :last_name,
+               :last_name_kana)
+               .merge(birth_date: birth_date)
+    end
   end
 
   # If you have extra params to permit, append them to the sanitizer.
-  def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  end
+  # def configure_account_update_params
+  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+  # end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
@@ -62,7 +67,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up for inactive accounts.
-  def after_inactive_sign_up_path_for(resource)
-    super(resource)
-  end
+  # def after_inactive_sign_up_path_for(resource)
+  #   super(resource)
+  # end
 end
