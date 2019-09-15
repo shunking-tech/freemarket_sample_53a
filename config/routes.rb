8e4c311858080a9f66f5b90ec9b92780f7bc4432
Nemo603 <nemotrace@gmail.com>
2019-09-15 19:40:18 +0900

    marge branch 'master' conflict-B

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'items#index'
-  resources :users, only: :show
+  resources :users, only: :show do
+    resources :creditcards, only: [:index, :new, :create]
+  end
+  get 'logout', to: 'users#logout'
 end
