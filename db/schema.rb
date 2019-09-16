b554052351494b0b934c6514909875d4deedb840
homarinn <homarinn1230@gmail.com>
2019-09-16 23:55:08 +0900

    jQuery

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_15_114111) do
+ActiveRecord::Schema.define(version: 2019_09_16_123900) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
