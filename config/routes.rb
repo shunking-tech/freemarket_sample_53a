dd8270ae6301cce7d077c28326bf2c3320eac154
homarinn <homarinn1230@gmail.com>
2019-09-23 03:46:30 +0900

    change route_name

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   }
 
   devise_scope :user do
-    get "sign_in", to: "users/sessions#new"
+    get "log_in", to: "users/sessions#new"
     get "sign_out", to: "users/sessions#destroy"
     get "registrations/sms_confirmation", to: "users/registrations#sms_confirmation"
     get "registrations/deliver_address", to: "users/registrations#deliver_address"
