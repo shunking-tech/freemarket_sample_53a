1464298e46fcdc4cfeeaee43f967e4107b17ddeb
seita_matsumoto <gyabooo@gmail.com>
2019-09-22 23:45:47 +0900

    ハッシュロケットの削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {
-    :registrations => 'users/registrations',
-    :sessions => 'users/sessions'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
   }
 
   devise_scope :user do
-    get "sign_in", :to => "users/sessions#new"
+    get "sign_in", to: "users/sessions#new"
     get "logout", to: "users/sessions#logout"
     delete "logout", to: "users/sessions#destroy"
   end
