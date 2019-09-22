dcbe04a4a0225ce67437c96b9f95ed5cb8d53003
seita_matsumoto <gyabooo@gmail.com>
2019-09-23 04:06:13 +0900

    stagingに必要そうな設定を一通り追加

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,3 +1,12 @@
+server '3.115.137.156', user: 'ec2-user', roles: %w{app db web}
+
+set :rails_env, "staging"
+set :unicorn_rack_env, "staging"
+set :branch, ask('input your branch (default: staging)', 'staging')
+set :application, 'stg_freemarket_sample_53a'
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['~/.ssh/stg_freemarket_sample_53a.pem']
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
