275dbf1a4167d3fc111464d5541abc7638cd8759
homarinn <homarinn1230@gmail.com>
2019-09-23 03:44:30 +0900

    setting routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,11 @@ Rails.application.routes.draw do
   devise_scope :user do
     get "sign_in", to: "users/sessions#new"
     get "sign_out", to: "users/sessions#destroy"
-    get "sms_confirmation", to: "users/registrations#sms_confirmation"
-    get "deliver_address", to: "users/registrations#deliver_address"
-    get "registration_complete", to: "users/registrations#registration_complete"
-    get "creditcard", to: "users/registrations#creditcard"
+    get "registrations/sms_confirmation", to: "users/registrations#sms_confirmation"
+    get "registrations/deliver_address", to: "users/registrations#deliver_address"
+    get "registrations/registration_complete", to: "users/registrations#registration_complete"
+    get "registrations/creditcard", to: "users/registrations#creditcard"
+    get "registrations/top", to: "users/registrations#top"
   end
 
   resources :users, only: [:show, :create] do
