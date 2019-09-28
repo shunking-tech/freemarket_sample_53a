bf3f6875bf3a4fc0ac9677a636c8e9b633bc987f
seita_matsumoto <gyabooo@gmail.com>
2019-09-28 15:48:47 +0900

    itemコントローラーのshowメソッド追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,15 @@
 class ItemsController < ApplicationController
+
   def index
+  end
+
+  def show
+    @item = Item.find(params[:id])
+    @item_comment = ItemComment.new
+    @item_comments = @item.item_comments
   end
 
   def new
   end
+
 end
