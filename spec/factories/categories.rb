284bf1e39455e9d74f36a8632a694947856b2bba
shunking-tech <shnktech@gmail.com>
2019-09-29 18:15:49 +0900

    factory_botの作成

---
+++ spec/factories/categories.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :category do
+    id                                {1}
+    name                              {"メンズ"}
+    ancestry                          {"aaa"}
+  end
+end
