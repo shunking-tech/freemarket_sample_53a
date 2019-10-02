fb20e2baca9576e688c50fc63ae4d8ca9dff1dff
homarinn <homarinn1230@gmail.com>
2019-10-02 13:59:54 +0900

    headerを部分テンプレート化

--- app/helpers/registration_helper.rb
+++ app/helpers/registration_helper.rb
@@ -4,6 +4,18 @@ module RegistrationHelper
     errors.present? ? "has-error" : ""
   end
 
+  def bar_lists
+    ["会員情報", "電話番号認証", "お届け先住所入力", "支払い方法", "完了"]
+  end
+
+  def bar_status(index, status_number)
+    if index == status_number
+      "active"
+    elsif index < status_number
+      "through"
+    end
+  end
+
   def years
     years = []
     (Time.current.year-119..Time.current.year).reverse_each do |year|
