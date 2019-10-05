65913b8584c42bbada7ecd82e87c01812cbf3498
mshr-Hub <masa.megane.616@gmail.com>
2019-10-06 01:27:26 +0900

    非推奨な文法を修正

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -6,7 +6,7 @@ module ControllerMacros
   end
 
   def login_user(user)
-    controller.stub(:authenticate_user!).and_return true
+    allow(controller).to receive(:authenticate_user!).and_return true
     @request.env["devise.mapping"] = Devise.mappings[:user]
     sign_in user
   end
