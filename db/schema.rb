4bf1a582329f2d26ee17ca0ed4cca10783523d9f
mshr-Hub <masa.megane.616@gmail.com>
2019-10-05 21:29:53 +0900

    コンフリクト解決後にmigrate

--- db/schema.rb
+++ db/schema.rb
@@ -69,6 +69,8 @@ ActiveRecord::Schema.define(version: 2019_09_29_064016) do
 
   create_table "sizes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "size", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
