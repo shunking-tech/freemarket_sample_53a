b89f83c21893f74c209f6263644254e49e23504e
mshr-Hub <masa.megane.616@gmail.com>
2019-10-08 21:08:25 +0900

    デコレータの誤字修正

--- app/decorators/item_decorator.rb
+++ app/decorators/item_decorator.rb
@@ -1,7 +1,7 @@
 class ItemDecorator < Draper::Decorator
   delegate_all
 
-  def player_expense_detail
+  def payer_expense_detail
     if payer_delivery_expense_i18n == Item.payer_delivery_expenses_i18n[:seller_burden]
       "#{payer_delivery_expense_i18n}(出品者負担)"
     elsif payer_delivery_expense_i18n == Item.payer_delivery_expenses_i18n[:buyer_burden]
