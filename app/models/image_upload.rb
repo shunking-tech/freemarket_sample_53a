986eeb71719cebe8e62baa2c187c67c10382bd22
seita_matsumoto <gyabooo@gmail.com>
2019-10-08 20:33:22 +0900

    不要なファイル削除

--- app/models/image_upload.rb
+++
@@ -1,3 +0,0 @@
-class ImageUpload < ApplicationRecord
-  mount_uploader :image, ImagesUploader
-end
--- db/migrate/20190923054217_create_image_uploads.rb
+++
@@ -1,8 +0,0 @@
-class CreateImageUploads < ActiveRecord::Migration[5.2]
-  def change
-    create_table :image_uploads do |t|
-      t.string :image
-      t.timestamps
-    end
-  end
-end
