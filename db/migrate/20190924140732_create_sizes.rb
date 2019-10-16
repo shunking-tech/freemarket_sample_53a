6ed2fba9ea9d8e6c07699ae86d567a800d724caa
mayumiasio <mayumayumayumi.11@gmail.com>
2019-10-16 21:58:30 +0900

    migrationファイル　削除(T . T)

--- db/migrate/20190924140732_create_sizes.rb
+++
@@ -1,9 +0,0 @@
-class CreateSizes < ActiveRecord::Migration[5.2]
-  def change
-    create_table :sizes do |t|
-      t.string :size, null:false
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20190924180040_create_item_likes.rb
+++
@@ -1,8 +0,0 @@
-class CreateItemLikes < ActiveRecord::Migration[5.2]
-  def change
-    create_table :item_likes do |t|
-
-      t.timestamps
-    end
-  end
-end
