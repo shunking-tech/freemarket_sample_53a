31d5759cde6e12e249430e6b50c31c0dbc1870b9
Nemo603 <nemotrace@gmail.com>
2019-10-16 19:22:49 +0900

    carrerwave設定 item_image

--- app/models/item_image.rb
+++ app/models/item_image.rb
@@ -1,3 +1,8 @@
 class ItemImage < ApplicationRecord
-  belongs_to :item
+  belongs_to :item, optional: true
+  mount_uploader :image, ImageUploader
+  
+  # mount_uploadersに変更すると複数投稿
+  # view画面に multiple: trueを追加で設定可能
+  # 一旦一枚の画像のみで設定
 end
