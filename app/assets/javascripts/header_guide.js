d3b20c8fdd517f8ee7e2c08f42465a0d86ab0859
seita_matsumoto <gyabooo@gmail.com>
2019-10-19 00:41:44 +0900

    box-shadowのオンオフ追加

--- app/assets/javascripts/header_guide.js
+++ app/assets/javascripts/header_guide.js
@@ -4,6 +4,15 @@ $(function () {
     var top_level_height = $(".ddmenu--top-level").height();
     $(".ddmenu--second-level").height(top_level_height);
     $(".ddmenu--third-level").height(top_level_height);
+
+    // box-shadowのオン・オフ
+    if ($(".breadcrumbs-wrap").length) {
+      $(".mercari-header").removeClass('mercari-header--box-shadow');
+    }
+    else {
+      $(".mercari-header").addClass('mercari-header--box-shadow');
+    }
+
   });
 
 })
