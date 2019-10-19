767f703b68f49c1316890a282e27ac1b1de122f6
mshr-Hub <masa.megane.616@gmail.com>
2019-10-19 16:22:59 +0900

    各アクション実行時に検証

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -1,9 +1,8 @@
 class TradesController < Users::CreditCardsController
   before_action :set_credit_card, :set_PAYJP_PRIVATE_KEY, :set_item
+  before_action :check_trades_validation
 
   def new
-    # 商品が出品中ではない、または出品者と購入者が同じ場合は商品詳細画面にredirect
-    redirect_to item_path(@item.id) if @item.user_id == current_user.id || ["waiting_shipping", "rating_seller", "rating_buyer", "complete", "stopping"].include?(@item.task) && @item.task != "exhibiting"
     if @credit_card.present?
       customer = Payjp::Customer.retrieve(@credit_card.customer_id)
       @default_card_information = customer.cards.retrieve(@credit_card.card_id)
@@ -34,5 +33,12 @@ class TradesController < Users::CreditCardsController
 
   def trade_params
     params.require(:trade).permit(:seller_id, :buyer_id, :item_id, :status)
+  end
+
+  def check_trades_validation
+    # 商品が出品中ではない、または出品者と購入者が同じ場合は商品詳細画面にredirect
+    if @item.user_id == current_user.id || ["waiting_shipping", "rating_seller", "rating_buyer", "complete", "stopping"].include?(@item.task) && @item.task != "exhibiting"
+      redirect_to item_path(@item.id)
+    end
   end
 end
