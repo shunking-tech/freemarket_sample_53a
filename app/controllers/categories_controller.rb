1fa28d5d5f713075da3e2daad3a99d0fbffa5b34
seita_matsumoto <gyabooo@gmail.com>
2019-10-20 22:50:17 +0900

    微修正

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,14 +5,11 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
+    @items = @category.belongs_items.page(params[:page]).per(PAGENATE)
     if @category.has_children?
       @categories = @category.children
-      @items = @category.indirects.present? ? 
-                Item.category_items(@category.indirects.ids).recently.page(params[:page]).per(PAGENATE) :
-                Item.category_items(@categories.ids).recently.page(params[:page]).per(PAGENATE)
     else
       @categories = @category.siblings
-      @items = Item.category_items(@category.id).recently.page(params[:page]).per(PAGENATE)
     end
   end
 
