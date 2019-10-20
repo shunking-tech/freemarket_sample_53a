6870cbec3730cd43570e29bfcea433863377cc3c
mshr-Hub <masa.megane.616@gmail.com>
2019-10-20 18:59:45 +0900

    バリデーションを修正

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -3,13 +3,13 @@ class Trade < ApplicationRecord
   belongs_to :buyer, class_name: "User"
   belongs_to :item
 
-  validates :seller_id, :buyer_id, :item_id, :status, presence: true
+  validates :status, presence: true
   validate :custom_trade_validate
 
   def custom_trade_validate
     if seller_id === buyer_id
-      errors.add(:seller_id, "購入者が同一人物です")
-      errors.add(:buyer_id, "出品者が同一人物です")
+      errors.add(:seller_id, "が同一人物です")
+      errors.add(:buyer_id, "が同一人物です")
     end
   end
 end
