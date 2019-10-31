02a62a9818676a4eb21b53899702c3cca2eb049a
seita_matsumoto <gyabooo@gmail.com>
2019-10-31 21:14:56 +0900

    微修正

--- app/controllers/users/listings_controller.rb
+++ app/controllers/users/listings_controller.rb
@@ -1,15 +1,15 @@
 class Users::ListingsController < ApplicationController
   def listing
-    @items = current_user.items.includes(:item_images).where(task: :exhibiting)
+    @items = current_user.items.includes(:item_images).recently.where(task: :exhibiting)
   end
 
   def in_progress
-    @items = current_user.items.includes(:item_images).where(task: [:waiting_shipping, :rating_seller, :rating_buyer])
+    @items = current_user.items.includes(:item_images).recently.where(task: [:waiting_shipping, :rating_seller, :rating_buyer])
     render :listing
   end
 
   def completed
-    @items = current_user.items.includes(:item_images).where(task: :complete)
+    @items = current_user.items.includes(:item_images).recently.where(task: :complete)
     render :listing
   end
 end
