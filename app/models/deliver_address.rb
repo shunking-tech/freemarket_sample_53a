8f86ac61d67e0b83684ea523b13de9dc2e4173e0
mshr-Hub <masa.megane.616@gmail.com>
2019-11-02 14:33:51 +0900

    配送先データの取得経路を修正

--- app/models/deliver_address.rb
+++ app/models/deliver_address.rb
@@ -1,5 +1,8 @@
 class DeliverAddress < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
   belongs_to :user
+  belongs_to_active_hash :prefecture
 
   validates_with DeliverAddressValidator
 end
